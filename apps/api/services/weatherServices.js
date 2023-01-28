3b4428507d2f50f44e79f6b3f0720074489fe8cb
Sean Patrick Wallace <wallace.sean111@gmail.com>
2023-01-28 23:17:58 +0100

    Add oneCall API

--- apps/api/services/weatherServices.js
+++ apps/api/services/weatherServices.js
@@ -6,8 +6,7 @@ const openWeatherKey = process.env.KEY_OPENWEATHER;
 console.log("process.env is: ", process.env);
 
 const getLocation = async (name, country, state) => {
-  
-  console.log('openWeatherKey is: ', openWeatherKey);
+  console.log("openWeatherKey is: ", openWeatherKey);
   let result;
   try {
     if (state && state !== "null") {
@@ -63,9 +62,10 @@ const getWeather = async (lat, long) => {
   let weather;
   try {
     weather = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${openWeatherKey}`
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&appid=${openWeatherKey}`
     );
     const weatherJSON = await weather.json();
+    console.log("weatherJSON is: ", weatherJSON);
     return weatherJSON;
   } catch (error) {
     console.log("Error getting weather: ", error.message);
@@ -91,7 +91,10 @@ const processImage = async (image) => {
   const buffer = Buffer.from(arrayBuffer);
   return buffer;
 };
-const createNewWeatherEntry = async (lat, long, mapTimestamp) => {
+const createNewWeatherEntry = async (params) => {
+  console.log("createNewWeatherEntry function called, params is: ", params);
+  const { name, country, state, lat, long } = params;
+  console.log("lat, long are: ", lat, long);
   const newEntry = new WeatherModel({});
   const weather = await getWeather(lat, long);
   //console.log('weather is: ', await weather);
@@ -101,37 +104,19 @@ const createNewWeatherEntry = async (lat, long, mapTimestamp) => {
   console.log("pollution.list is: ", pollution.list);
 
   // populates the new entry with data from weather and pollution APIs
-  newEntry.cityId = weather.id;
-  newEntry.latitude = weather.coord.lat;
-  newEntry.longitude = weather.coord.lon;
-  newEntry.timestamp = Date.now();
-  newEntry.cityName = weather.name;
-  newEntry.country = weather.sys.country;
-  newEntry.state = weather.sys.state;
-  newEntry.weatherId = weather.weather[0].id;
-  newEntry.description = weather.weather[0].description;
-  newEntry.temp = weather.main.temp;
-  newEntry.temp_min = weather.main.temp_min;
-  newEntry.temp_max = weather.main.temp_max;
-  newEntry.pressure = weather.main.pressure;
-  newEntry.humidity = weather.main.humidity;
-  newEntry.wind_speed = weather.wind.speed;
-  newEntry.wind_deg = weather.wind.deg;
-  newEntry.wind_gust = weather.wind.gust;
-  newEntry.clouds = weather.clouds.all;
-  newEntry.sunrise = weather.sys.sunrise;
-  newEntry.sunset = weather.sys.sunset;
+  newEntry.cityName = name;
+  newEntry.country = country;
+  newEntry.state = state;
+  newEntry.latitude = lat;
+  newEntry.longitude = long;
   newEntry.timezone = weather.timezone;
-  newEntry.aqi = pollution.list[0].main.aqi;
-  newEntry.co = pollution.list[0].components.co;
-  newEntry.no = pollution.list[0].components.no;
-  newEntry.no2 = pollution.list[0].components.no2;
-  newEntry.ozone = pollution.list[0].components.o3;
-  newEntry.so2 = pollution.list[0].components.so2;
-  newEntry.pm25 = pollution.list[0].components.pm2_5;
-  newEntry.pm10 = pollution.list[0].components.pm10;
-  newEntry.nh3 = pollution.list[0].components.nh3;
-
+  newEntry.timezoneOffset = weather.timezone_offset;
+  newEntry.timestamp = Date.now();
+  newEntry.current = weather.current;
+  newEntry.hourly = weather.hourly;
+  newEntry.daily = weather.daily;
+  newEntry.alerts = weather.alerts;
+  newEntry.pollution = pollution;
   console.log("New entry assembled! newEntry is: ", newEntry);
   const savedEntry = await newEntry.save();
   return savedEntry;
@@ -139,10 +124,17 @@ const createNewWeatherEntry = async (lat, long, mapTimestamp) => {
 
 export const processPOST = async (name, country, state) => {
   console.log("processPOST function called");
-  console.log('openWeatherKey is: ', openWeatherKey);
+  console.log("openWeatherKey is: ", openWeatherKey);
   const location = await getLocation(name, country, state);
   console.log("location is: ", location);
   const locationJSON = await location.json();
+  const params = {
+    name: name,
+    country: country,
+    state: state,
+    lat: locationJSON[0].lat,
+    long: locationJSON[0].lon,
+  };
   console.log("locationJSON[0] is: ", locationJSON[0]);
   console.log("locationJSON[0].lat is: ", locationJSON[0].lat);
   console.log("locationJSON[0].lon   is: ", locationJSON[0].lon);
@@ -162,10 +154,7 @@ export const processPOST = async (name, country, state) => {
     console.log("Date.now() - 900000 is: ", Date.now() - 900000);
     if (checkEntry[0].timestamp < Date.now() - 900000) {
       console.log("Entry is older than 15 minutes. Updating entry.");
-      const updatedEntry = await createNewWeatherEntry(
-        locationJSON[0].lat,
-        locationJSON[0].lon
-      );
+      const updatedEntry = await createNewWeatherEntry(params);
       return updatedEntry;
     } else {
       console.log("Entry is younger than 15 minutes. Returning entry.");
@@ -174,8 +163,7 @@ export const processPOST = async (name, country, state) => {
   } else {
     console.log("DB entry does not exist. Creating new entry.");
     const savedEntry = await createNewWeatherEntry(
-      locationJSON[0].lat,
-      locationJSON[0].lon
+      params
     );
     console.log("savedEntry is: ", savedEntry);
     return savedEntry;
