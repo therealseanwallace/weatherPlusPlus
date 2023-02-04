b365e0369c62c4fec7cc0c5f724374ce54b2284c
Sean Patrick Wallace <wallace.sean111@gmail.com>
2023-02-04 17:09:31 +0100

    Continue building hourly forecasts

--- routes/weatherRouter.js
+++ routes/weatherRouter.js
@@ -1,25 +1,25 @@
 import express from "express";
-import { processPOST } from "../services/weatherServices.js";
+import processPOST from "../services/weatherServices.js";
 
 const router = express.Router();
 
-router.get('/name/:name/country/:country/state/:state', async (req, res) => {
-  console.log('GET request received. req.params is ', req.params);
+router.get("/name/:name/country/:country/state/:state", async (req, res) => {
+  console.log("GET request received. req.params is ", req.params);
   const { name, country, state } = req.params;
   const result = await processPOST(name, country, state);
-  console.log('router! result is: ', result);
+  console.log("router! result is: ", result);
   res.send(result);
 });
 
-router.get('/name/:name', async (req, res) => {
-  console.log('GET request received. req.params is ', req.params);
+router.get("/name/:name", async (req, res) => {
+  console.log("GET request received. req.params is ", req.params);
   const { name } = req.params;
   const result = await processPOST(name);
   res.send(result);
 });
 
-router.get('/name/:name/country/:country', async (req, res) => {
-  console.log('GET request received. req.params is ', req.params);
+router.get("/name/:name/country/:country", async (req, res) => {
+  console.log("GET request received. req.params is ", req.params);
   const { name, country } = req.params;
   const result = await processPOST(name, country);
   res.send(result);
